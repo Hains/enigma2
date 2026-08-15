81aee1b28ef737786b3968a923dcaddd3843239c
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-08-15 15:34:59 +0200

    Softcsa using the streamrelay file as well

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -144,10 +144,11 @@ def reload_whitelist_bouquets():
 reload_whitelist_vbi()
 reload_whitelist_bouquets()
 
-class InfoBarWhitelists:
+class InfoBarStreamRelay:
 
-	def __init__(self, FILENAME):
-		self.FILENAME = FILENAME
+	FILENAME = "/etc/enigma2/whitelist_streamrelay"
+
+	def __init__(self):
 		self.__srefs = self.__sanitizeData(open(self.FILENAME, 'r').readlines()) if os.path.isfile(self.FILENAME) else []
 
 	def __sanitizeData(self, data):
@@ -182,6 +183,7 @@ class InfoBarWhitelists:
 
 	def streamrelayChecker(self, playref):
 		is_stream_relay = False
+		config.misc.softcam_use_softcsa.value = False
 		if config.softcsa.useStreamRelayWhitelist.value:
 			playrefstring, renamestring = self.splitref(playref.toString())
 			if '%3a//' not in playrefstring and playrefstring in self.__srefs:
@@ -192,30 +194,16 @@ class InfoBarWhitelists:
 					playrefmod = playrefstring
 				playref = eServiceReference("%s%s%s:%s" % (playrefmod, url.replace(":", "%3a"), playrefstring.replace(":", "%3a"), renamestring or ServiceReference(playref).getServiceName()))
 				is_stream_relay = True
+				config.misc.softcam_use_softcsa.value = True
 				print(f"[{self.__class__.__name__}] Play service {playref.toString()} via streamrelay")
 				playref.setCompareSref(playrefstring, True)
 		return playref, is_stream_relay
-
-	def softCSAChecker(self, playref):
-		if config.misc.softcam_softcsa.value == 1:
-			playrefstring, renamestring = self.splitref(playref.toString())
-			playref = eServiceReference("%s:%s" % (playrefstring.replace(":", "%3a"), renamestring or ServiceReference(playref).getServiceName()))
-			if playrefstring in self.__srefs:
-				print(f"[{self.__class__.__name__}] Play service {playref.toString()} via softcsa")
-				config.misc.softcam_use_softcsa.value = True
-			else:
-				print(f"[{self.__class__.__name__}] Play service {playref.toString()} via hw descrambling")
-				config.misc.softcam_use_softcsa.value = False
-		else:
-			config.misc.softcam_use_softcsa.value = False
-		return
 
 	def checkService(self, service):
 		return service and self.splitref(service.toString())[0] in self.__srefs
 
 
-streamrelay = InfoBarWhitelists("/etc/enigma2/whitelist_streamrelay")
-softcsa = InfoBarWhitelists("/etc/enigma2/whitelist_softcsa")
+streamrelay = InfoBarStreamRelay()
 
 
 class subservice:
