9a363d4673b63b9e7893721b5420ad3587275cb7
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-08-16 21:16:04 +0200

    fix

--- lib/service/servicedvbrecord.h
+++ lib/service/servicedvbrecord.h
@@ -79,6 +79,7 @@ private:
 	ePtr<eDVBCSASession> m_csa_session;
 	bool m_use_software_descramble;
 	bool csa_is_auto() { return eConfigManager::getConfigIntValue("config.misc.softcam_softcsa", 0) == 2; }
+	bool csa_from_whitelist() { return eConfigManager::getConfigBoolValue("config.misc.softcam_use_softcsa", false) == true; }
 
 	int setupSoftwareDescrambler(eDVBServicePMTHandler::program& program);
 
