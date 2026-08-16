e3453748c556e829eeea1f0203942f3a4080c321
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-08-16 15:27:46 +0200

    fix

--- lib/service/servicedvbstream.h
+++ lib/service/servicedvbstream.h
@@ -64,6 +64,7 @@ protected:
 	void setupSpeculativeDescrambler();
 	void cleanupCSASession();
 	bool csa_is_auto() { return eConfigManager::getConfigIntValue("config.misc.softcam_softcsa", 0) == 2; }
+	bool csa_from_whitelist() { return eConfigManager::getConfigBoolValue("config.misc.softcam_use_softcsa", false) == true; }
 };
 
 #endif
